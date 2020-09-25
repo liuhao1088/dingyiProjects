f37010d1928834517158c22ade4107f6bda39263
liuhao1088 <1350569467@qq.com>
2020-09-25 11:15:21 +0800

    更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -48,6 +48,11 @@ Page({
       url: "../brandthree/brandthree"
     })
   } ,
+  detailsdj: function (e) {
+    wx.navigateTo({
+      url: "../details/details"
+    })
+  } ,
 
   /**
    * 生命周期函数--监听页面加载
